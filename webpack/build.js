688c86d6a67102a14b1196732f975fbf68fda3d8
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-11-02 15:28:04 +0800

    optimization.minimizer

--- webpack/build.js
+++ webpack/build.js
@@ -2,6 +2,7 @@
 
 import path from 'path';
 import postcssPresetEnv from 'postcss-preset-env';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import OptimizeCssAssetsWebpackPlugin from 'optimize-css-assets-webpack-plugin';
@@ -36,7 +37,6 @@ export default {
       filename: 'css/[name].css',
       chunkFilename: 'css/[name].css'
     }),
-    new OptimizeCssAssetsWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: path.join(__dirname, '../public/index.html'),
       path: outputDir,
@@ -116,6 +116,51 @@ export default {
     ]
   },
   optimization: {
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          parse: {
+            // we want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minfication steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending futher investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            // eslint-disable-next-line
+            ascii_only: true
+          }
+        },
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        parallel: true,
+        cache: true,
+        sourceMap: false
+      }),
+      new OptimizeCssAssetsWebpackPlugin()
+    ],
     runtimeChunk: {
       name: 'runtime'
     },
