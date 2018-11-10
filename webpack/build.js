2add5186d03a367f597a5e8536afedf62523b114
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-11-10 18:43:38 +0800

    used parallel-webpack;

--- webpack/build.js
+++ webpack/build.js
@@ -1,217 +1,221 @@
 /** @format */
 
-import webpack from 'webpack';
-import path from 'path';
-import postcssPresetEnv from 'postcss-preset-env';
-import TerserWebpackPlugin from 'terser-webpack-plugin';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import OptimizeCssAssetsWebpackPlugin from 'optimize-css-assets-webpack-plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import InlineChunksHTMLWebpackPlugin from 'inline-chunks-html-webpack-plugin';
-import { config } from 'dotenv';
-import chalk from 'chalk';
+const webpack = require('webpack');
+const path = require('path');
+const postcssPresetEnv = require('postcss-preset-env');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const InlineChunksHTMLWebpackPlugin = require('inline-chunks-html-webpack-plugin');
+const dotenv = require('dotenv');
 
-config();
-
-const BUILD_TARGET = process.env.BUILD_TARGET.trim();
-const outputDir = path.join(__dirname, '../builds/' + BUILD_TARGET);
-const entryFile = path.join(__dirname, '../src/' + BUILD_TARGET + '/entry.js');
-
-console.log(chalk.yellow('Using'), chalk.magenta(outputDir), chalk.yellow('as output directory.')); // eslint-disable-line
-console.log(chalk.yellow('Using'), chalk.magenta(entryFile), chalk.yellow('as entry file.')); // eslint-disable-line
-console.log(); // eslint-disable-line
+dotenv.config();
 
 process.env.NODE_ENV = 'production';
 
+const BUILD_TARGETS = process.env.BUILD_TARGETS.split(',').map(str => str.trim());
 const envs = ['PUBLIC_PATH', 'NODE_ENV'];
+const configs = [];
 
-export default {
-  name: BUILD_TARGET,
-  mode: 'production',
-  bail: true,
-  entry: entryFile,
-  output: {
-    filename: 'js/[name].js',
-    chunkFilename: 'js/[name].js',
-    path: outputDir
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      env: Object.keys(process.env).reduce((compiled, key) => {
-        if (key.indexOf('APP_ENV_') !== -1 || envs.includes(key)) {
-          compiled[key] = JSON.stringify(process.env[key]);
+for (let a = 0; a < BUILD_TARGETS.length; a++) {
+  configs.push(configure(BUILD_TARGETS[a]));
+}
+
+function configure(buildTarget) {
+  const outputDir = path.join(__dirname, '../builds/' + buildTarget);
+  const entryFile = path.join(__dirname, '../src/' + buildTarget + '/entry.js');
+
+  return {
+    name: buildTarget,
+    mode: 'production',
+    bail: true,
+    entry: entryFile,
+    output: {
+      filename: 'js/[name].js',
+      chunkFilename: 'js/[name].js',
+      path: outputDir
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        env: Object.keys(process.env).reduce((compiled, key) => {
+          if (key.indexOf('APP_ENV_') !== -1 || envs.includes(key)) {
+            compiled[key] = JSON.stringify(process.env[key]);
+          }
+
+          return compiled;
+        }, {})
+      }),
+      new HTMLWebpackPlugin({
+        template: path.join(__dirname, '../public/index.html'),
+        path: outputDir,
+        filename: 'index.html',
+        title: process.env.WEBSITE_NAME,
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true
         }
-
-        return compiled;
-      }, {})
-    }),
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, '../public/index.html'),
-      path: outputDir,
-      filename: 'index.html',
-      title: process.env.WEBSITE_NAME,
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true
-      }
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-      chunkFilename: 'css/[name].css'
-    }),
-    new InlineChunksHTMLWebpackPlugin({
-      deleteFile: true,
-      inlineChunks: ['runtime.js']
-    }),
-    new CopyWebpackPlugin([{ from: path.join(__dirname, '../public'), ignore: ['index.html'] }])
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'webpack-loader-clean-pragma',
-            options: {
-              pragmas: [
-                {
-                  start: '/** @delete */',
-                  end: '/** @enddelete */'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+        chunkFilename: 'css/[name].css'
+      }),
+      new InlineChunksHTMLWebpackPlugin({
+        deleteFile: true,
+        inlineChunks: ['runtime.js']
+      }),
+      new CopyWebpackPlugin([{ from: path.join(__dirname, '../public'), ignore: ['index.html'] }])
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'webpack-loader-clean-pragma',
+              options: {
+                pragmas: [
+                  {
+                    start: '/** @delete */',
+                    end: '/** @enddelete */'
+                  }
+                ],
+                consoles: {
+                  warns: true
                 }
-              ],
-              consoles: {
-                warns: true
               }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.scss/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.join(__dirname, '../src/shared/styles')]
+          ]
+        },
+        {
+          test: /\.css/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.scss/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.join(__dirname, '../src/shared/styles')]
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [postcssPresetEnv()]
+              }
             }
-          },
-          {
-            loader: 'postcss-loader',
+          ]
+        },
+        {
+          test: /\.ttf|\.woff2|\.woff|\.eot/,
+          use: {
+            loader: 'file-loader',
             options: {
-              plugins: [postcssPresetEnv()]
+              name: 'fonts/[name].[ext]',
+              publicPath: '/'
             }
-          }
-        ]
-      },
-      {
-        test: /\.ttf|\.woff2|\.woff|\.eot/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'fonts/[name].[ext]',
-            publicPath: '/'
-          }
-        }
-      },
-      {
-        test: /\.gif|\.png|\.jpg|\.jpeg/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'images/[name].[ext]',
-            publicPath: '/'
-          }
-        }
-      },
-      {
-        test: /\.svg/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'svgs/[name].[ext]',
-            publicPath: '/'
-          }
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new TerserWebpackPlugin({
-        terserOptions: {
-          parse: {
-            // we want terser to parse ecma 8 code. However, we don't want it
-            // to apply any minfication steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
-            ecma: 8
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-            // Disabled because of an issue with Terser breaking valid code:
-            // https://github.com/facebook/create-react-app/issues/5250
-            // Pending futher investigation:
-            // https://github.com/terser-js/terser/issues/120
-            inline: 2
-          },
-          mangle: {
-            safari10: true
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            // eslint-disable-next-line
-            ascii_only: true
           }
         },
-        // Use multi-process parallel running to improve the build speed
-        // Default number of concurrent runs: os.cpus().length - 1
-        parallel: true,
-        cache: true,
-        sourceMap: false
-      }),
-      new OptimizeCssAssetsWebpackPlugin()
-    ],
-    runtimeChunk: {
-      name: 'runtime'
+        {
+          test: /\.gif|\.png|\.jpg|\.jpeg/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+              publicPath: '/'
+            }
+          }
+        },
+        {
+          test: /\.svg/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: 'svgs/[name].[ext]',
+              publicPath: '/'
+            }
+          }
+        }
+      ]
     },
-    splitChunks: {
-      chunks: 'all'
+    optimization: {
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            parse: {
+              // we want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minfication steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              // https://github.com/facebook/create-react-app/pull/4234
+              ecma: 8
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              // https://github.com/facebook/create-react-app/issues/2376
+              // Pending further investigation:
+              // https://github.com/mishoo/UglifyJS2/issues/2011
+              comparisons: false,
+              // Disabled because of an issue with Terser breaking valid code:
+              // https://github.com/facebook/create-react-app/issues/5250
+              // Pending futher investigation:
+              // https://github.com/terser-js/terser/issues/120
+              inline: 2
+            },
+            mangle: {
+              safari10: true
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              // Turned on because emoji and regex is not minified properly using default
+              // https://github.com/facebook/create-react-app/issues/2488
+              // eslint-disable-next-line
+              ascii_only: true
+            }
+          },
+          // Use multi-process parallel running to improve the build speed
+          // Default number of concurrent runs: os.cpus().length - 1
+          parallel: true,
+          cache: true,
+          sourceMap: false
+        }),
+        new OptimizeCssAssetsWebpackPlugin()
+      ],
+      runtimeChunk: {
+        name: 'runtime'
+      },
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    resolve: {
+      alias: {
+        _shared: path.join(__dirname, '../src/shared')
+      }
+    },
+    node: {
+      dgram: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      // eslint-disable-next-line
+      child_process: 'empty'
     }
-  },
-  resolve: {
-    alias: {
-      _shared: path.join(__dirname, '../src/shared')
-    }
-  },
-  node: {
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    // eslint-disable-next-line
-    child_process: 'empty'
-  }
-};
+  };
+}
+
+module.exports = configs;
--- webpack/filesizes.js
+++ webpack/filesizes.js
@@ -1,74 +1,77 @@
 /** @format */
 
-import path from 'path';
-import fs from 'fs';
-import gzipSize from 'gzip-size';
-import chalk from 'chalk';
-import { config } from 'dotenv';
+const path = require('path');
+const fs = require('fs');
+const gzipSize = require('gzip-size');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
 
-config();
+dotenv.config();
 
-const BUILD_TARGET = process.env.BUILD_TARGET.trim();
-const outputDir = path.join(__dirname, '../builds/' + BUILD_TARGET);
-let filesizes;
-let directories;
+const BUILD_TARGETS = process.env.BUILD_TARGETS.split(',').map(str => str.trim());
 
-console.log('-----------------------'); // eslint-disable-line
-console.log(BUILD_TARGET, ': File sizes after GZIP'); // eslint-disable-line
-console.log('-----------------------'); // eslint-disable-line
+for (let a = 0; a < BUILD_TARGETS.length; a++) {
+  const outputDir = path.join(__dirname, '../builds/' + BUILD_TARGETS[a]);
+  let filesizes;
+  let directories;
 
-filesizes = [];
+  filesizes = [];
 
-directories = [
-  {
-    name: 'css',
-    path: path.join(outputDir, 'css')
-  },
-  {
-    name: 'js',
-    path: path.join(outputDir, 'js')
-  }
-];
+  directories = [
+    {
+      name: 'css',
+      path: path.join(outputDir, 'css')
+    },
+    {
+      name: 'js',
+      path: path.join(outputDir, 'js')
+    }
+  ];
 
-Promise.all(
-  directories
-    .map(dir =>
-      new Promise(resolve => {
-        fs.readdir(dir.path, (err, files) => {
-          resolve({
-            err,
-            files
+  Promise.all(
+    directories
+      .map(dir =>
+        new Promise(resolve => {
+          fs.readdir(dir.path, (err, files) => {
+            resolve({
+              err,
+              files
+            });
           });
-        });
-      }).then(response => {
-        if (!response.err) {
-          return Promise.all(
-            response.files.map(file =>
-              gzipSize.file(path.join(dir.path, file)).then(fsize => {
-                filesizes.push({
-                  file: `${BUILD_TARGET}/${dir.name}/${file}`,
-                  size: (fsize / 1024).toFixed(2),
-                  unit: 'KB'
-                });
-              })
-            )
-          );
-        }
+        }).then(response => {
+          if (!response.err) {
+            return Promise.all(
+              response.files.map(file =>
+                gzipSize.file(path.join(dir.path, file)).then(fsize => {
+                  filesizes.push({
+                    file: `${BUILD_TARGETS[a]}/${dir.name}/${file}`,
+                    size: (fsize / 1024).toFixed(2),
+                    unit: 'KB'
+                  });
+                })
+              )
+            );
+          }
 
-        return Promise.resolve();
-      })
-    )
-    .concat(
-      gzipSize.file(path.join(outputDir, 'index.html')).then(fsize => {
-        filesizes.push({
-          file: BUILD_TARGET + '/index.html',
-          size: (fsize / 1024).toFixed(2)
-        });
-      })
-    )
-).then(() => {
-  filesizes.forEach(filesize => {
-    // eslint-disable-next-line
-    console.log(`${chalk.yellow(filesize.file)}: ${chalk.magenta(`${filesize.size} KB`)}`);
+          return Promise.resolve();
+        })
+      )
+      .concat(
+        gzipSize.file(path.join(outputDir, 'index.html')).then(fsize => {
+          filesizes.push({
+            file: BUILD_TARGETS[a] + '/index.html',
+            size: (fsize / 1024).toFixed(2)
+          });
+        })
+      )
+  ).then(() => {
+    console.log('-----------------------'); // eslint-disable-line
+    console.log(BUILD_TARGETS[a], ': File sizes after GZIP'); // eslint-disable-line
+    console.log('-----------------------'); // eslint-disable-line
+
+    filesizes.forEach(filesize => {
+      // eslint-disable-next-line
+      console.log(`${chalk.yellow(filesize.file)}: ${chalk.magenta(`${filesize.size} KB`)}`);
+    });
   });
-});
+}
